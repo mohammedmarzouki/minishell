81eab213dd1cdb8ac3c6944223232b1355831d71
Taoufiq Jmari <jmaritaoufiq@gmail.com>
2021-02-10 14:34:02 +0100

    get_next_line

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: tjmari <tjmari@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 16:33:25 by tjmari            #+#    #+#             */
-/*   Updated: 2021/02/09 17:38:20 by tjmari           ###   ########.fr       */
+/*   Updated: 2021/02/10 14:32:24 by tjmari           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,17 @@
 
 int		main(int argc, char **argv, char **env)
 {
+	char	*input;
+
 	(void)argc;
 	(void)argv;
-	printf("Minishell\n");
-	while (*env)
-		printf("%s\n", *env++);
+	(void)env;
+	while (1)
+	{
+		ft_putstr_fd("\033[1;33mminishell$\033[0m ", 1);
+		get_next_line(0, &input);
+		ft_putendl_fd(input, 1);
+		free(input);
+	}
 	return (0);
 }
