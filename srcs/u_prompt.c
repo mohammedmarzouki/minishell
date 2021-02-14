4ff7e7d7f7c12c0d7bb4aa8a149ea8fe56b23ecb
Taoufiq Jmari <jmaritaoufiq@gmail.com>
2021-02-14 12:51:49 +0100

    1. Put the prompt in a seperate function! 2. exit command!

--- srcs/u_prompt.c
+++ srcs/u_prompt.c
@@ -1,37 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.c                                        :+:      :+:    :+:   */
+/*   u_prompt.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tjmari <tjmari@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/09 16:33:25 by tjmari            #+#    #+#             */
-/*   Updated: 2021/02/14 12:48:23 by tjmari           ###   ########.fr       */
+/*   Created: 2021/02/14 12:37:31 by tjmari            #+#    #+#             */
+/*   Updated: 2021/02/14 12:39:50 by tjmari           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int		main(int argc, char *argv[], char *envp[])
+void    ps1(void)
 {
-	char	*input;
-
-	(void)argc;
-	(void)argv;
-	(void)envp;
-	while (1)
-	{
-		ps1();
-		get_next_line(0, &input);
-		if (ft_strncmp(input, "exit\0", 5) == 0)
-		{
-			free(input);
-			break;
-		}
-		ft_putendl_fd(input, 1);
-		// parsing();
-		// executing();
-		free(input);
-	}
-	return (0);
+    ft_putstr_fd("\033[1;33mminishell$\033[0m ", 1);
 }
