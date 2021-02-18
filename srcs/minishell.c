ed51dde836f4940f94a6f8a9ea03b88325efb898
Taoufiq Jmari <jmaritaoufiq@gmail.com>
2021-02-18 17:31:44 +0100

    Into execution

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,31 +6,53 @@
 /*   By: tjmari <tjmari@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 16:33:25 by tjmari            #+#    #+#             */
-/*   Updated: 2021/02/14 12:48:23 by tjmari           ###   ########.fr       */
+/*   Updated: 2021/02/18 17:28:11 by tjmari           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int		main(int argc, char *argv[], char *envp[])
+void	tj_cmd(void)
+{
+	g_cmd[0].cmd = "exit";
+	g_cmd[0].args = "15";
+	g_cmd[0].flg = NULL;
+	g_cmd[0].sep = NULL;
+}
+
+void	executing(void)
+{
+	if (getcwd(g_tool.cwd, sizeof(g_tool.cwd)) == NULL)
+		perror("getcwd() error");
+	if (ft_strncmp(g_cmd[0].cmd, "echo", 4) == 0)
+		ft_echo();
+	else if (ft_strncmp(g_cmd[0].cmd, "cd", 2) == 0)
+		ft_cd();
+	else if (ft_strncmp(g_cmd[0].cmd, "pwd", 3) == 0)
+		ft_pwd();
+	else if (ft_strncmp(g_cmd[0].cmd, "export", 6) == 0)
+		ft_export();
+	else if (ft_strncmp(g_cmd[0].cmd, "env", 3) == 0)
+		ft_env();
+	else if (ft_strncmp(g_cmd[0].cmd, "exit", 4) == 0)
+		ft_exit();
+	return ;
+}
+
+int		main(int argc, char **argv, char **envp)
 {
 	char	*input;
 
 	(void)argc;
 	(void)argv;
-	(void)envp;
+	g_tool.envp = envp;
+	tj_cmd();
 	while (1)
 	{
 		ps1();
 		get_next_line(0, &input);
-		if (ft_strncmp(input, "exit\0", 5) == 0)
-		{
-			free(input);
-			break;
-		}
-		ft_putendl_fd(input, 1);
 		// parsing();
-		// executing();
+		executing();
 		free(input);
 	}
 	return (0);
