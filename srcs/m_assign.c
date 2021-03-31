1e5641afb7bdde666b8f3b5f1b7d315385e34a1d
Mohammed Marzouki <mmarzouk@e1r4p6.1337.ma>
2021-03-31 14:09:47 +0100

    to do

--- srcs/m_assign.c
+++ srcs/m_assign.c
@@ -6,27 +6,21 @@
 /*   By: mmarzouk <mmarzouk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 16:48:56 by mmarzouk          #+#    #+#             */
-/*   Updated: 2021/03/19 16:27:27 by mmarzouk         ###   ########.fr       */
+/*   Updated: 2021/03/30 10:55:59 by mmarzouk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-// void	check_redi(char **sp, int *i)
-// {
-// 	int		j;
+void	redis(char **sp, int *i)
+{
+	int		j;
 
-// 	j = 0;
-// 	while (sp[*i][j])
-// 	{
-// 		if (sp[*i][j] == '>' && sp[(*i)++][j] == '>')
-// 			append_in();
-// 		else if (sp[*i][j] == '>')
-// 			put_in();
-// 		else if (sp[*i][j] == '<')
-// 			take_from();
-// 	}
-// }
+	j = 0;
+	while (sp[*i][j] && (sp[*i][j] == '<' || sp[*i][j] == '>'))
+		j++;
+	
+}
 
 // void	check_args(char **sp)
 // {
@@ -36,18 +30,18 @@
 void	assign(char **sp)
 {
 	int		i;
-	// int		j;
+	int		j;
 	
 	i = 0;
 	while (sp[i])
 	{
-		// j = 0;
-		// while (sp[i][j] == ' ')
-		// 	j++;
-		// if(sp[i][j] == '<' || sp[i][j] == '>')
-		// 	check_redi(sp, &i);
-		// else
-		// 	check_args(sp);
+		j = 0;
+		while (sp[i][j] == ' ')
+			j++;
+		if(sp[i][j] == '<' || sp[i][j] == '>')
+			redis(sp, &i);
+		else
+			add_args(sp);
 		printf("|%s|\n", sp[i]);
 		i++;
 	}
--- srcs/m_parsing.c
+++ srcs/m_parsing.c
@@ -6,7 +6,7 @@
 /*   By: mmarzouk <mmarzouk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/18 09:15:36 by mmarzouk          #+#    #+#             */
-/*   Updated: 2021/03/19 16:35:49 by mmarzouk         ###   ########.fr       */
+/*   Updated: 2021/03/27 11:27:14 by mmarzouk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,13 @@ static void single_q(char *s,int *i)
 			break;
 	}
 }
+
 char    **split_it(char *s)
 {
     char **sp;
     int i;
     short flag;
+    short 
     int start;
 
     flag = 0;
@@ -44,12 +46,19 @@ char    **split_it(char *s)
         else if (s[i] == '"' && flag) {
             flag = 0;
         }
-        else if((s[i] == ' ' || s[i] == '|' || s[i] == ';' || s[i] == '>' || s[i] == '<') && !flag)
+        else if((s[i] == ' ' || s[i] == '|' || s[i] == ';' ) && flag != 1)
         {
+            flag = 0;
             sp = append_line(sp,ft_substr(s,start,(i - start)));
-            start = i;
 			while (s[i] && s[i + 1] == ' ' && s[i] == ' ')
 				i++;
+            start = i;
+		}
+        else if((s[i] == '>' || s[i] == '<' ) && !flag)
+        {
+            flag = 2;
+            sp = append_line(sp,ft_substr(s,start,(i - start)));
+            start = i;
 		}
         if(s[i] == '\\' && s[i+1]) {
             i += 2;
